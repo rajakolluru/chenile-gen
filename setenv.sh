aa7d9a2ab7454995f31659603d286b087a937ca9
Raja Shankar Kolluru <raja@thekollurus.com>
2025-01-29 14:51:09 +0530

    Minor alterations in custom xml workflow generation

--- setenv.sh
+++
@@ -1,1 +0,0 @@
-export PATH=$PATH:$PWD/app-gen/bin:$PWD/stmcli/bin
---
+++ setpath.sh
@@ -0,0 +1,3 @@
+# source this file i.e. run it using "source setpath.sh" to make sure that stm-cli and gen.sh are in the path.
+# change your .zshrc or .bashrc accordingly as well
+export PATH=$PATH:$PWD/app-gen/bin:$PWD/stmcli/bin
