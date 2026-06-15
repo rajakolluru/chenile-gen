aee4b3c17c7da04ad92840ce5e969f0a48a329bf
Raja Shankar Kolluru <raja@thekollurus.com>
2026-06-15 10:50:41 +0530

    changes to add info

--- app-gen/config/setenv.sh
+++ app-gen/config/setenv.sh
@@ -1,5 +1,5 @@
 export chenilePackage=org.chenile
-export chenileVersion=2.0.11
+export chenileVersion=2.1.24
 export chenileBddVersion=0.1.1
 
 export com=com
@@ -9,4 +9,3 @@ export org=myorg
 export defaultServiceName="serviceName"
 export defaultVersion="0.0.1-SNAPSHOT"
 export defaultDestFolder="./output"
-
